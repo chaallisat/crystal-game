6f897bfe6c2f58bd76baf2b25db3db6b8eaffcb8
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-07 16:25:58 -0700

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+//start JS
+$(document).ready(function() {
+
+//set variables
+let win = 0;
+let loss = 0;
+let enemy = 0
+
+
+//Start game function
+function game() {
+
+//computer shows a random number between 19-120
+    enemy = Math.floor(Math.random() * 120) + 19;
+
+
+
+}
+
+
+
+
+
+
+})
+//crystal is assigned a random number between 1-12
+//player's score starts at 0
+//each click will add to player's score
+//if computer number = player score, player wins
+//if player score > computer number, player loses
+//game ends
+//game restarts
+//new random numbers
+//count total wins and losses
